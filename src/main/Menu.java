8ac59473fe825fb588d9c847d19eecc8937a7c1d
nslang18 <nslang18@iastate.edu>
2018-11-01 14:03:36 -0500

    centered buttons on screen

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -18,6 +18,7 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import scenes.MainMenu;
 
 public class Menu extends Application {
 
@@ -33,21 +34,7 @@ public class Menu extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
 		
-		//create elements for the main menu scene and then add them to the scene
-		Label label_title = new Label("Platform the Platformer");
-		Button button_start = new Button("Start");
-		Button button_settings = new Button("Settings");
-		Button button_highscores = new Button("Highscores");
-		Button button_exit = new Button("Exit");
-		VBox layout1 = new VBox(5);
-		layout1.getChildren().addAll(label_title, button_start, button_settings, button_highscores, button_exit);
-		
-		//create and set background image for the main menu
-		Image mm_bg_img = SwingFXUtils.toFXImage(ImageIO.read(getClass().getResource("/backgrounds/mm_background.png")), null);
-		BackgroundImage mainMenuBackground = new BackgroundImage(mm_bg_img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-		layout1.setBackground(new Background(mainMenuBackground));
-		
-		mainMenu = new Scene(layout1, 800, 600);
+		mainMenu = new MainMenu(800, 600).getScene();
 		
 		//set the first scene for the window as well as setting its title
 		window.setScene(mainMenu);
---
+++ src/scenes/MainMenu.java
@@ -0,0 +1,65 @@
+package scenes;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+public class MainMenu {
+	
+	Scene scene;
+	
+	public MainMenu(int screenW, int screenH){
+		//create elements for the main menu scene and then add them to the scene
+		Button button_start = createButton("Start");
+		Button button_settings = createButton("Settings");
+		Button button_highscores = createButton("Highscores");
+		Button button_exit = createButton("Exit");
+		
+		GridPane GP = new GridPane();
+		VBox VB = new VBox(10);
+		VB.getChildren().addAll(button_start, button_settings, button_highscores, button_exit);
+		GP.getChildren().add(VB);
+		GP.setAlignment(Pos.CENTER);
+				
+		//create and set background image for the main menu
+		Image mm_bg_img;
+		try {
+			mm_bg_img = SwingFXUtils.toFXImage(ImageIO.read(getClass().getResource("/backgrounds/mm_background.png")), null);
+			BackgroundImage mainMenuBackground = new BackgroundImage(mm_bg_img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+			GP.setBackground(new Background(mainMenuBackground));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		scene = new Scene(GP, screenW, screenH);
+
+	}
+	
+	public Scene getScene(){
+		return scene;
+	}
+	
+	private Button createButton(String text){
+		Button b = new Button(text);
+		b.setPrefSize(150, 50);
+		
+		return b;
+	}
+
+}
