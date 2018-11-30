b34ee8f13b1086cf56beacd1ad3dc463b35b2c6d
tjfrye <tjfrye@iastate.edu>
2018-11-30 13:48:56 -0500

    Added database referencing upon login

--- src/scenes/Login.java
+++ src/scenes/Login.java
@@ -1,10 +1,8 @@
 package scenes;
 
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
-import javafx.embed.swing.SwingFXUtils;
+import javafx.embed.swing.SwingFXUtils; 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,13 +16,21 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import database.Queries;
+import sprite.Player;
 
 public class Login extends Menu{
+	
+	Label label_attempt;
 	
 	public Login(int screenW, int screenH, Stage window){
 		super(screenW, screenH, window);
 		//create elements for the main menu scene and then add them to the scene
+		label_attempt = new Label("Please Login:");
+		label_attempt.setFont(new Font("Arial", 20));
+		
 		Label label_name = new Label("Username:");
 		TextField text_name = new TextField();
 		
@@ -32,14 +38,14 @@ public class Login extends Menu{
 		TextField text_password = new TextField();
 		
 		Button button_login = createButton("Login");
-		button_login.setOnAction(e -> {window.setScene(new MainMenu(width, height, window).getScene());});
+		button_login.setOnAction(e -> loginAttempt(text_name.getCharacters().toString(), text_password.getCharacters().toString()));
 		
 		Button button_back = createButton("Back");
 		button_back.setOnAction(e -> {window.setScene(new StartMenu(width, height, window).getScene());});
 		
 		GridPane GP = new GridPane();
 		VBox VB = new VBox(10);
-		VB.getChildren().addAll(label_name, text_name, label_password, text_password, button_login, button_back);
+		VB.getChildren().addAll(label_attempt, label_name, text_name, label_password, text_password, button_login, button_back);
 		GP.getChildren().add(VB);
 		GP.setAlignment(Pos.CENTER);
 				
@@ -48,8 +54,15 @@ public class Login extends Menu{
 		GP.setBackground(new Background(new BackgroundImage (mm_bg_img, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
 		
 		s = new Scene(GP, screenW, screenH);
-		
-		
-
+	}
+	
+	public void loginAttempt(String username, String password) {
+		if(Queries.login(username, password) != 0) {
+			label_attempt.setText("Login Successful");
+			Player.currentUsername = username;
+			window.setScene(new MainMenu(width, height, window).getScene());
+		} else {
+			label_attempt.setText("Failed to Login");
+		}
 	}
 }
